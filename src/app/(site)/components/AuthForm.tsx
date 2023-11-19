dd8c48d72863a8c7f81da4d43aae8ea1f1d5038d
gurielamp <gurielamp@gmail.com>
2023-11-20 06:41:04 +0700

    add github login

--- src/app/(site)/components/AuthForm.tsx
+++ src/app/(site)/components/AuthForm.tsx
@@ -7,6 +7,8 @@ import { useForm, FieldValues, SubmitHandler } from "react-hook-form"
 import AuthSocialButton from "./AuthSocialButton"
 import { BsGithub, BsGoogle } from "react-icons/bs"
 import axios from "axios"
+import toast from "react-hot-toast"
+import { signIn } from "next-auth/react"
 
 type Variant = "LOGIN" | "REGISTER"
 
@@ -38,15 +40,44 @@ export default function AuthForm() {
     setIsLoading(true)
 
     if (variant === "REGISTER") {
-      axios.post("/api/register", data)
+      axios
+        .post("/api/register", data)
+        .catch(() => toast.error("Something went wrong!"))
+        .finally(() => setIsLoading(false))
     }
 
     if (variant === "LOGIN") {
+      signIn("credentials", {
+        ...data,
+        redirect: false,
+      })
+        .then((callback) => {
+          if (callback?.error) {
+            toast.error("Invalid credentials!")
+          }
+          if (callback?.ok && !callback?.error) {
+            toast.success("Logged in!")
+          }
+        })
+        .finally(() => setIsLoading(false))
     }
   }
 
   const socialAction = (action: string) => {
     setIsLoading(true)
+
+    signIn(action, {
+      redirect: false,
+    })
+      .then((callback) => {
+        if (callback?.error) {
+          toast.error("Invalid Credentials!")
+        }
+        if (callback?.ok && !callback?.error) {
+          toast.success("Logged in!")
+        }
+      })
+      .finally(() => setIsLoading(false))
   }
 
   return (
