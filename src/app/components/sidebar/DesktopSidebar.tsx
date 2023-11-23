a5d4412b61bd98642a1cfc97215de6b6c6bfafa2
gurielamp <gurielamp@gmail.com>
2023-11-23 17:53:44 +0700

    add img/placeholder

--- src/app/components/sidebar/DesktopSidebar.tsx
+++ src/app/components/sidebar/DesktopSidebar.tsx
@@ -3,10 +3,18 @@
 import useRoutes from "@/app/hooks/useRoutes"
 import { useState } from "react"
 import DesktopItem from "./DesktopItem"
+import { User } from "@prisma/client"
+import Avatar from "../Avatar"
 
-const DesktopSidebar = () => {
+interface DesktopSidebarProps {
+  currentUser: User
+}
+
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
   const routes = useRoutes()
   const [isOpen, setIsOpen] = useState(false)
+
+  console.log({ currentUser })
 
   return (
     <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
@@ -23,6 +31,14 @@ const DesktopSidebar = () => {
             />
           ))}
         </ul>
+      </nav>
+      <nav className="mt-4 flex flex-col justify-between items-center">
+        <div
+          onClick={() => setIsOpen(true)}
+          className="cursor-pointer hover:opacity-75 transition"
+        >
+          <Avatar user={currentUser} />
+        </div>
       </nav>
     </div>
   )
--- src/app/hooks/useRoutes.ts
+++ src/app/hooks/useRoutes.ts
@@ -1,13 +1,13 @@
 import { useMemo } from "react";
 import { usePathname } from "next/navigation";
-import { HiChat } from "react-icons/hi";
-import { HiArrowLeftOnRectangle, HiUsers } from "react-icons/hi2";
+import { HiChat } from 'react-icons/hi';
+import { HiArrowLeftOnRectangle, HiUsers } from 'react-icons/hi2';
 import { signOut } from "next-auth/react";
 import useConversation from "./useConversation";
 
 const useRoutes = () => {
-  const pathname = usePathname()
-  const { conversationId } = useConversation()
+  const pathname = usePathname();
+  const { conversationId } = useConversation();
 
   const routes = useMemo(() => [
     {
@@ -24,12 +24,13 @@ const useRoutes = () => {
     },
     {
       label: 'Logout',
+      onClick: () => signOut(),
       href: '#',
       icon: HiArrowLeftOnRectangle,
-      onClick: () => signOut()
     }
-  ], [pathname, conversationId])
-  return routes
-}
+  ], [pathname, conversationId]);
 
-export default useRoutes
+  return routes;
+};
+
+export default useRoutes;
